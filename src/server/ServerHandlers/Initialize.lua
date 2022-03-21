5c16b928b6c5149d616a9a2c51a04f6a2a51ffe9
Son Nguyen Hoang <sonnguyen9800@gmail.com>
2022-03-21 22:08:46 +0700

    Update Initialize Module

---
+++ src/server/ServerHandlers/Initialize.lua
@@ -0,0 +1,38 @@
+local playersServices = game:GetService("Players")
+local dataMod = require(script.Parent.Data)
+local defineModule = require(script.Parent.Define)
+local spawnParts = workspace.SpawnParts
+
+local inititalizeModule = {}
+
+
+local function getStage(stageNum)
+    if spawnParts == nil then
+        return nil
+    end
+
+    for _, stagePart in pairs(spawnParts:GetChildren()) do
+        
+        if stagePart.Stage.Value == stageNum then
+            return stagePart
+        end
+    end
+    
+end
+
+playersServices.PlayerAdded:Connect(function(player)
+    
+    player.CharacterAdded:Connect(function(char)
+        
+        local stageNum = dataMod.get(player, defineModule.StageName);
+        local spawnPoint = getStage(stageNum);
+        if spawnPoint then
+            char:SetPrimaryPartCFrame(spawnPoint.CFrame * CFrame.new(0,3,0));
+        end
+    end)
+end)
+
+
+
+return inititalizeModule
+
