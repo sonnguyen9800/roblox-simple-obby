85344ecc2896692f888379cb34372456bd00a0b1
Son Nguyen Hoang <sonnguyen9800@gmail.com>
2022-03-21 23:22:48 +0700

    Init Monetization

---
+++ src/server/ServerHandlers/Monetization.lua
@@ -0,0 +1,15 @@
+local insertService = game:GetService("InsertService")
+local marketplaceService = game:GetService("MarketplaceService")
+
+local dataStoreService = game:GetService("DataStoreService")
+local playerServices = game:GetService("Players")
+
+local defineModule = require(script.Parent.Define)
+local dataModule = require(script.Parent.Data)
+
+local monetizationModule = {}
+
+
+
+
+return monetizationModule
