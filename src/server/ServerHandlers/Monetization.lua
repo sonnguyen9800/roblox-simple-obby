9538e7260c39eb971211d1f36dcb8f77cf1325f6
Son Nguyen Hoang <sonnguyen9800@gmail.com>
2022-03-26 08:36:39 +0700

    Update

--- src/server/ServerHandlers/Monetization.lua
+++ src/server/ServerHandlers/Monetization.lua
@@ -3,13 +3,47 @@ local marketplaceService = game:GetService("MarketplaceService")
 
 local dataStoreService = game:GetService("DataStoreService")
 local playerServices = game:GetService("Players")
-
+local collectionService = game:GetService("CollectionService")
 local defineModule = require(script.Parent.Define)
 local dataModule = require(script.Parent.Data)
-
+local monetizationDefine = require(script.Parent.MonetizationDefine)
 local monetizationModule = {}
 
 
+monetizationModule.insertTool = function(player, assestId)
+    local assest = insertService:LoadAsset(assestId)
+    local tool = assest:FindFirstChildOfClass(assest);
+
+    tool.Parent = player.Backpack
+    assest:Destroy()
+    
+end
 
 
+monetizationModule[monetizationDefine.SpeedCoil.GamePassUrl] = function(player)
+    
+    monetizationModule.insertTool(player, monetizationDefine.SpeedCoil.Id)
+end
+monetizationModule[monetizationDefine.GravityCoil.GamePassUrl] = function(player)
+    
+    monetizationModule.insertTool(player, monetizationDefine.GravityCoil.Id)
+end
+monetizationModule[monetizationDefine.Radio.GamePassUrl] = function(player)
+    
+    monetizationModule.insertTool(player, monetizationDefine.Radio.Id)
+end
+
+monetizationModule[monetizationDefine.Coinsx100.GamePassUrl] = function(player)
+    
+    dataModule.increment(player, defineModule.CoinName, 100)
+end
+
+
+
+marketplaceService.PromptGamePassPurchaseFinished:Connect(function(player, gamePassId, wasPurchased)
+    if wasPurchased then
+        collectionService:AddTag(player, gamePassId)
+        monetizationModule[gamePassId](player)
+    end
+end)
 return monetizationModule
---
+++ src/server/ServerHandlers/MonetizationDefine.lua
@@ -0,0 +1,24 @@
+
+local MonetizationDefine = {
+    SpeedCoil = {
+        Id = 1,
+        GamePassUrl = ""
+    },
+
+    GravityCoil = {
+        Id = 2,
+        GamePassUrl = ""
+    },
+
+    Radio = {
+        Id = 3,
+        GamePassUrl = ""
+    },
+
+    Coinsx100 = {
+        id = 4,
+        GamePassUrl = ""
+    }
+}
+
+return MonetizationDefine;
