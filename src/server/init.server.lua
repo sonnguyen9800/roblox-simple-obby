706b01eb0b6095edefd27124084b72cb3fe3087e
sonnguyen <sonnguyen9800@gmail.com>
2022-03-31 13:29:07 +0700

    Update

--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,1 +1,7 @@
-print("Hello world, from server!")
+for _, module in pairs(script:GetChildren()) do
+	local loadMod = coroutine.create(function()
+		require(module)
+	end)
+	
+	coroutine.resume(loadMod)
+end
